a39222f7833e4970c0a1bd95d8193fb8665110e3
Shanjjeev-Lab <shanjjeevrohan2911@gmail.com>
2024-09-13 20:29:26 +0530

    Python Number-Swap ACP

---
+++ main.py
@@ -0,0 +1,21 @@
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+
+print( )
+
+print("Original Values: ")
+
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+
+a, b, c = c, b, a
+
+print( )
+
+print("Swapped Values: ")
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
